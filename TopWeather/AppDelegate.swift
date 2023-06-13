43523bca214cdb6c12b1a3459514a4e228f13e66
mozeXezom <79330736+mozeXezom@users.noreply.github.com>
2023-06-13 16:04:33 +0300

    Initial Commit

---
+++ TopWeather/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TopWeather
+//
+//  Created by mozeX on 13.06.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
